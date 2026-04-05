5e2673b847836d25e3ca3a2ff9a3254b313cbb42
Abhivansh <31abhivanshj@gmail.com>
2026-04-05 17:21:29 +0530

    feat: add initial definition and starter functions

--- spin_lock/src/main.rs
+++ spin_lock/src/main.rs
@@ -1,5 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+
+pub struct SpinLock {
+    locked: AtomicBool,
 }
 
+impl SpinLock {
+    pub fn new() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+        }
+    }
 
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
+pub fn main() {
+
+}
