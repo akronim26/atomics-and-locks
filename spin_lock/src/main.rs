6cc7fb148c8e519a6530b9aa238bc0358c81ca39
Abhivansh <31abhivanshj@gmail.com>
2026-04-05 17:50:07 +0530

    chore: adjust functions for unsafe value

--- spin_lock/src/main.rs
+++ spin_lock/src/main.rs
@@ -4,11 +4,11 @@ use std::sync::atomic::Ordering::{Acquire, Release};
 
 pub struct SpinLock<T> {
     locked: AtomicBool,
-    value: UnsafeCell<T>
+    value: UnsafeCell<T>,
 }
 
+// We dont need to implement Sync because our lock will only allow one thread to access the data at a time unlike any reader-writer lock
 unsafe impl<T> Sync for SpinLock<T> where T: Send {}
-
 
 impl<T> SpinLock<T> {
     pub fn new(value: T) -> Self {
@@ -19,18 +19,21 @@ impl<T> SpinLock<T> {
     }
 
     // We use Acquire and Release to ensure that whatever was intended to happen during the last time it was locked has already happened
-    pub fn lock(&self) {
+    // By making the lifetime safe, we ensure that we have the returned reference for the same lifetime as the lock
+    pub fn lock<'a>(&'a self) -> &'a mut T {
         while self.locked.swap(true, Acquire) {
             // Tells the processor that we are spinning while waiting for something to change
             std::hint::spin_loop();
         }
+        unsafe { &mut *self.value.get() }
     }
 
-    pub fn unlock(&self) {
+    // The reference to the T must be gone by the time this is called
+    // Because if it is not gone and the function is called, then another thread can fetch the mutable reference and can 
+    // create two mutable references at a time which is invalid
+    pub unsafe fn unlock(&self) {
         self.locked.store(false, Release);
     }
 }
 
-pub fn main() {
-
-}
+pub fn main() {}
