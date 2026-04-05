b9a6f41c9b626b916ff1e40a68485b2d6e32228b
Abhivansh <31abhivanshj@gmail.com>
2026-04-05 17:36:19 +0530

    feat: add value field to share between threads

--- spin_lock/src/main.rs
+++ spin_lock/src/main.rs
@@ -1,19 +1,27 @@
+use std::cell::UnsafeCell;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Release};
 
-pub struct SpinLock {
+pub struct SpinLock<T> {
     locked: AtomicBool,
+    value: UnsafeCell<T>
 }
 
-impl SpinLock {
-    pub fn new() -> Self {
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+
+impl<T> SpinLock<T> {
+    pub fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
         }
     }
 
+    // We use Acquire and Release to ensure that whatever was intended to happen during the last time it was locked has already happened
     pub fn lock(&self) {
         while self.locked.swap(true, Acquire) {
+            // Tells the processor that we are spinning while waiting for something to change
             std::hint::spin_loop();
         }
     }
