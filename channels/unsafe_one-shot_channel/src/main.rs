1113e38a6992a466f53cea34ec9fc9cefe8a46a8
Abhivansh <31abhivanshj@gmail.com>
2026-04-23 17:36:50 +0530

    chore: add Drop to OneShotChannel

--- channels/unsafe_one-shot_channel/src/main.rs
+++ channels/unsafe_one-shot_channel/src/main.rs
@@ -2,10 +2,12 @@ use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Release, Relaxed};
+use std::thread;
 
 // We use MaybeUninit because initially the queue is empty and don't contain any value
 pub struct OneShotChannel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
+    in_use: AtomicBool,
     item_ready: AtomicBool,
 }
 
@@ -17,13 +19,17 @@ impl<T> OneShotChannel<T> {
     pub fn new() -> Self {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()),
+            in_use: AtomicBool::new(false),
             item_ready: AtomicBool::new(false),
         }
     }
 
     // Only call this function once
-    pub unsafe fn send(&self, message: T) {
-        (*self.message.get()).write(message);
+    pub fn send(&self, message: T) {
+        if self.in_use.swap(true, Acquire) {
+            panic!("Channel is already in use");
+        }
+        unsafe { (*self.message.get()).write(message) };
         self.item_ready.store(true, Release);
     }
 
@@ -38,4 +44,28 @@ impl<T> OneShotChannel<T> {
         }
         unsafe { (*self.message.get()).assume_init_read() }
     }
+}
+
+impl<T> Drop for OneShotChannel<T> {
+    fn drop(&mut self) {
+        if *self.item_ready.get_mut() {
+            unsafe { (*self.message.get()).assume_init_drop() };
+        }
+    }
+}
+
+// The thread is parked until the other thread sends the message and the other thread unparks it
+fn main() {
+    let channel = OneShotChannel::new();
+    let t = thread::current();
+    thread::scope(|s| {
+        s.spawn(|| {
+            channel.send("Hello World");
+            t.unpark();
+        });
+        while !channel.is_ready() {
+            thread::park();
+        }
+        assert_eq!(channel.receive(), "Hello World");
+    })
 }
