e71ac402cdce1e5c12ef55e0139206cf764d9c35
Abhivansh <31abhivanshj@gmail.com>
2026-05-01 01:12:32 +0530

    feat: implement arc

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,6 +1,6 @@
 use std::ops::Deref;
 use std::ptr::NonNull;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, fence, Ordering::{Acquire, Relaxed}};
 
 struct ArcData<T> {
     ref_count: AtomicUsize,
@@ -32,12 +32,45 @@ impl<T> Arc<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe { self.ptr.as_ref() }
     }
+
+    // We can get a mutable reference only when we have a single reference
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc.data().ref_count.load(Relaxed) == 1 {
+            Some(unsafe { &mut arc.ptr.as_mut().data })
+        } else {
+            None
+        }
+    }
+
 }
 
+// We don't implement DerefMut here because Arc is not mutable
+// and hence we don't to give mutable reference
 impl<T> Deref for Arc<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
         &self.data().data
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        // TODO: Handle overflow
+        self.data().ref_count.fetch_add(1, Relaxed);
+        Arc { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    // fence ensures that the reordering does not happen when only one thread contains T
+    // we have not used AcqRel here because we only want to ensure Acquire for the last thread
+    fn drop(&mut self) {
+        if self.data().ref_count.fetch_sub(1, Relaxed) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
     }
 }
