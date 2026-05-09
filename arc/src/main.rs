320b740cabb5e4f3e14152b05769f912affec32d
Abhivansh <31abhivanshj@gmail.com>
2026-05-10 01:49:19 +0530

    chore: basic implementation of Arc with Weak pointers

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,31 +1,46 @@
+use std::cell::UnsafeCell;
 use std::ops::Deref;
 use std::ptr::NonNull;
+use std::rc::Weak;
 use std::sync::atomic::{AtomicUsize, fence, Ordering::{Acquire, Relaxed}};
 
 struct ArcData<T> {
-    ref_count: AtomicUsize,
-    data: T,
+    // Number of `Arc`s
+    data_ref_count: AtomicUsize,
+    // Number of `Arc`s and `Weak`s
+    alloc_ref_count: AtomicUsize,
+    // The actual data is optional because it may be only held by `Weak`
+    data: UnsafeCell<Option<T>>,
+}
+
+pub struct Arc<T> {
+    weak: Weak<T>
 }
 
 // A NonNull pointer is a pointer which cannot be null
-pub struct Arc<T> {
+pub struct Weak<T> {
     ptr: NonNull<ArcData<T>>,
 }
 
 // Sync is required to maintain T in sync with all the threads
 // Send is required for the last thread to actually drop T
-unsafe impl<T: Send + Sync> Send for Arc<T> {}
-unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+unsafe impl<T: Send + Sync> Send for Weak<T> {}
+unsafe impl<T: Send + Sync> Sync for Weak<T> {}
 
 impl<T> Arc<T> {
     // Box::leak prevents automatic dropping of the data
     // This is done because Arc wants manual control
     pub fn new(data: T) -> Self {
         Arc {
-            ptr: NonNull::from(Box::leak(Box::new(ArcData {
-                ref_count: AtomicUsize::new(1),
-                data,
-            }))),
+            weak: Weak {
+                ptr: NonNull::from({
+                    Box::leak(Box::new(ArcData{
+                        data_ref_count: AtomicUsize::new(1),
+                        alloc_ref_count: AtomicUsize::new(1),
+                        data: UnsafeCell::new(Some(data))
+                    }))
+                })
+            }
         }
     }
 
@@ -36,7 +51,7 @@ impl<T> Arc<T> {
     // We can get a mutable reference only when we have a single reference
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
         if arc.data().ref_count.load(Relaxed) == 1 {
-            Some(unsafe { &mut arc.ptr.as_mut().data })
+            Some(unsafe { arc.weak.ptr.as_mut().data.get_mut().as_mut().unwrap() })
         } else {
             None
         }
@@ -44,21 +59,52 @@ impl<T> Arc<T> {
 
 }
 
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe {
+            self.ptr.as_ref()
+        }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        self.data().alloc_ref_count.fetch_add(1, Relaxed);
+        Weak {
+            ptr: self.ptr
+        }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_sub(1, Relaxed) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
+
 // We don't implement DerefMut here because Arc is not mutable
 // and hence we don't to give mutable reference
 impl<T> Deref for Arc<T> {
     type Target = T;
 
-    fn deref(&self) -> &T {
-        &self.data().data
+    fn deref(&self) -> Option<T> {
+        unsafe {
+            *self.weak.data().data.get().as_ref().unwrap()
+        }
     }
 }
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
         // TODO: Handle overflow
-        self.data().ref_count.fetch_add(1, Relaxed);
-        Arc { ptr: self.ptr }
+        let weak = self.weak.clone();
+        self.data().data_ref_count.fetch_add(1, Relaxed);
+        Arc { weak }
     }
 }
 
@@ -69,7 +115,7 @@ impl<T> Drop for Arc<T> {
         if self.data().ref_count.fetch_sub(1, Relaxed) == 1 {
             fence(Acquire);
             unsafe {
-                drop(Box::from_raw(self.ptr.as_ptr()));
+                *self.weak.data().data.get() = None
             }
         }
     }
