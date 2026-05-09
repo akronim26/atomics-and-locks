669674bae3f0d70db3dd980faab645ed45c1729a
Abhivansh <31abhivanshj@gmail.com>
2026-05-10 02:22:42 +0530

    feat: add upgrade and downgrade fn to Arc

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -1,7 +1,6 @@
 use std::cell::UnsafeCell;
 use std::ops::Deref;
 use std::ptr::NonNull;
-use std::rc::Weak;
 use std::sync::atomic::{AtomicUsize, fence, Ordering::{Acquire, Relaxed}};
 
 struct ArcData<T> {
@@ -45,16 +44,20 @@ impl<T> Arc<T> {
     }
 
     fn data(&self) -> &ArcData<T> {
-        unsafe { self.ptr.as_ref() }
+        unsafe { self.weak.ptr.as_ref() }
     }
 
     // We can get a mutable reference only when we have a single reference
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
-        if arc.data().ref_count.load(Relaxed) == 1 {
+        if arc.data().data_ref_count.load(Relaxed) == 1 {
             Some(unsafe { arc.weak.ptr.as_mut().data.get_mut().as_mut().unwrap() })
         } else {
             None
         }
+    }
+
+    pub fn downgrade(&self) -> Weak<T> {
+        self.weak.clone()
     }
 
 }
@@ -63,6 +66,29 @@ impl<T> Weak<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe {
             self.ptr.as_ref()
+        }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        // We loop here because if the process failed due to a race condition, then it can be 
+        // re-executed. 
+        loop {
+            if n == 0 {
+                return None;
+            }
+            // The `compare_exchange_weak` ensures that if the current value of `data_ref_count`
+            // is still `n` then change it to `n + 1` as a `Weak` pointer is being converted to 
+            // `Arc`
+            if let Err(e) = self.data().data_ref_count.compare_exchange_weak(n, n + 1, Relaxed, Relaxed) {
+                // If another thread simultaneously upgraded another `Weak` pointer due to which this 
+                // lead to an error here, then e is the new value of the n
+                n = e;
+                continue;
+            }
+            return Some(Arc{
+                weak: self.clone()
+            })
         }
     }
 }
@@ -92,9 +118,9 @@ impl<T> Drop for Weak<T> {
 impl<T> Deref for Arc<T> {
     type Target = T;
 
-    fn deref(&self) -> Option<T> {
+    fn deref(&self) -> &T {
         unsafe {
-            *self.weak.data().data.get().as_ref().unwrap()
+            &(*self.weak.data().data.get()).as_ref().unwrap()
         }
     }
 }
@@ -112,7 +138,7 @@ impl<T> Drop for Arc<T> {
     // fence ensures that the reordering does not happen when only one thread contains T
     // we have not used AcqRel here because we only want to ensure Acquire for the last thread
     fn drop(&mut self) {
-        if self.data().ref_count.fetch_sub(1, Relaxed) == 1 {
+        if self.data().data_ref_count.fetch_sub(1, Relaxed) == 1 {
             fence(Acquire);
             unsafe {
                 *self.weak.data().data.get() = None
